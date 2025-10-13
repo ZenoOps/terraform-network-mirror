022a3861624f46ab9214ded7485176cc26b65005
ZenoOps <kyawsoe.thet27@gmail.com>
2025-10-13 20:45:49 +0700

    Modifying the bucket.tf with detailed description

--- bucket.tf
+++ bucket.tf
@@ -1,11 +1,12 @@
 resource "aws_s3_bucket" "provider_cache" {
-  bucket = "provider-cache-tf-2"
-  # force_destroy = true ///Allow the deletion of an S3 bucket even when it contains objects. Use with caution.
+  bucket = "provider-cache-tf"
+  force_destroy = true # We need this to run "terraform destroy" without prompting error "bucket is not empty" if the bucket has objects in it.
   tags = {
-    Name = "provider-cache-tf-2"
+    Name = "provider-cache-tf"
   }
 }
 
+# Blocking all public access to the S3 bucket.
 resource "aws_s3_bucket_public_access_block" "provider_cache_bucket" {
   bucket = aws_s3_bucket.provider_cache.id
 
@@ -30,8 +31,8 @@ data "aws_iam_policy_document" "allow_cloudfront" {
     resources = [
       "${aws_s3_bucket.provider_cache.arn}/*"
     ]
-
-    condition {
+    # This block only allows access if the request comes from the specified CloudFront distribution.
+    condition {   
       test     = "StringEquals"
       variable = "AWS:SourceArn"
       values   = [aws_cloudfront_distribution.mirror.arn] 
@@ -43,3 +44,11 @@ resource "aws_s3_bucket_policy" "allow_cloudfront" {
   bucket = aws_s3_bucket.provider_cache.id
   policy = data.aws_iam_policy_document.allow_cloudfront.json
 }
+
+# Uncomment the following block if you want to enable versioning on the S3 bucket.
+# resource "aws_s3_bucket_versioning" "provider_cache" {
+#   bucket = aws_s3_bucket.provider_cache.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
